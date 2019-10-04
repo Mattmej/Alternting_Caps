e46c52cfb58808c1e01a6695aacb59e366760098
matt <mattcmej@gmail.com>
2019-10-04 09:51:42 -0700

    Add react-bootstrap to project.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Needed for react-bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 function App() {
   return (
     <div className="App">
